e6c062dca92d1d52246504591a18951f99db6b27
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-21 10:11:24 -0700

    init

---
+++ Formula/glitter.rb
@@ -0,0 +1,11 @@
+class Glitter < Formula
+    desc "Git tooling of the future."
+    homepage "https://github.com/Milo123459/glitter"
+    url "https://github.com/Milo123459/glitter/releases/download/v1.1.1/glitter-x86_64-apple-darwin.tar.gz"
+    sha256 ""
+    version "1.1.1"
+  
+    def install
+      bin.install "tle"
+    end
+  end
