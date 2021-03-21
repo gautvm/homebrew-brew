e017aea441f0739923c0047b7c8bc8acb9f9a3ad
Milo <50248166+Milo123459@users.noreply.github.com>
2021-03-21 18:54:06 +0000

    Update glitter.rb

--- Formula/glitter.rb
+++ Formula/glitter.rb
@@ -2,7 +2,7 @@ class Glitter < Formula
     desc "Git tooling of the future"
     homepage "https://github.com/Milo123459/glitter"
     url "https://github.com/Milo123459/glitter/releases/download/v1.1.1/glitter-x86_64-apple-darwin.tar.gz"
-    sha256 ""
+    sha256 "01afaa674d6cc137c56e9aa2df3a08447c2e17155d16698256c4231c75e14944"
     version "1.1.1"
   
     def install
