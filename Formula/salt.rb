0bd1e9c24b34645c5b298afe70b09c3d874ddf45
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-14 09:36:15 +0100

    Update salt.rb

--- Formula/salt.rb
+++ Formula/salt.rb
@@ -2,8 +2,8 @@ class Salt < Formula
     desc "Simple and fast task management via the CLI"
     homepage "https://github.com/Milo123459/salt"
     url "https://github.com/Milo123459/salt/releases/latest/download/salt-x86_64-apple-darwin.tar.gz"
-    sha256 "7658173574e04c9d010d0e244ce9d56242cc442837c88e52ca321e417d11d767"
-    version "0.1.0"
+    sha256 "dfa86061055fbc6742b64df2d9aa3d6559fce2270eecbe7d6cdf43888a0440e4"
+    version "0.1.1"
   
     def install
       bin.install "salt"
